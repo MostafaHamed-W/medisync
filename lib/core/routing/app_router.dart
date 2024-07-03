7a6f140dfede83860f3035f27db508031c88e2a7
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-07-03 21:57:44 +0300

    Setting up Routes, AppRouter, OnBoardingScreen and LoginScreen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:medisync/core/routing/routes.dart';
+import 'package:medisync/features/login/login_screen.dart';
+import 'package:medisync/features/onboarding/onboarding_screen.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
