c89d0935a19906be14953a1c8a2bf73fb8782641
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-07-03 21:20:20 +0300

    Adding project folder structure

---
+++ lib/medisync.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MediSync extends StatelessWidget {
+  const MediSync({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
