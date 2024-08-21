290773be140512935a9c12eefe885c0c5c3086c2
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-08-21 14:33:55 +0300

    Added networking layer

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:medisync/core/networking/api_constants.dart';
+import 'package:medisync/features/login/data/models/login_request_body.dart';
+import 'package:medisync/features/login/data/models/login_response.dart';
+import 'package:retrofit/retrofit.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponse> login(
+    @Body() LoginRequestBody loginRequestBody,
+  );
+}
