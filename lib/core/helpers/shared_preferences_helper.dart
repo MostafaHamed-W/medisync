374d5264db4727a07c4673654e8c71f64bf291b0
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-09-29 17:44:00 +0300

    Added shared preferences helper class

---
+++ lib/core/helpers/shared_preferences_helper.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefHelper {
+  // private constructor as I don't want to allow creating an instance of this class itself.
+  SharedPrefHelper._();
+
+  /// Removes a value from SharedPreferences with given [key].
+  static removeData(String key) async {
+    debugPrint('SharedPrefHelper : data with key : $key has been removed');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.remove(key);
+  }
+
+  /// Removes all keys and values in the SharedPreferences
+  static clearAllData() async {
+    debugPrint('SharedPrefHelper : all data has been cleared');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.clear();
+  }
+
+  /// Saves a [value] with a [key] in the SharedPreferences.
+  static setData(String key, value) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    debugPrint("SharedPrefHelper : setData with key : $key and value : $value");
+    switch (value.runtimeType) {
+      case String:
+        await sharedPreferences.setString(key, value);
+        break;
+      case int:
+        await sharedPreferences.setInt(key, value);
+        break;
+      case bool:
+        await sharedPreferences.setBool(key, value);
+        break;
+      case double:
+        await sharedPreferences.setDouble(key, value);
+        break;
+      default:
+        return null;
+    }
+  }
+
+  /// Gets a bool value from SharedPreferences with given [key].
+  static getBool(String key) async {
+    debugPrint('SharedPrefHelper : getBool with key : $key');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getBool(key) ?? false;
+  }
+
+  /// Gets a double value from SharedPreferences with given [key].
+  static getDouble(String key) async {
+    debugPrint('SharedPrefHelper : getDouble with key : $key');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getDouble(key) ?? 0.0;
+  }
+
+  /// Gets an int value from SharedPreferences with given [key].
+  static getInt(String key) async {
+    debugPrint('SharedPrefHelper : getInt with key : $key');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getInt(key) ?? 0;
+  }
+
+  static getString(String key) async {
+    debugPrint('SharedPrefHelper : getString with key : $key');
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getString(key) ?? '';
+  }
+}
